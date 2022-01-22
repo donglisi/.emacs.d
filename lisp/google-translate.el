144ae1670f4e7fd4b4881c0da27abb08da1eedff
Dongli Si <kgdboc@gmail.com>
2022-01-22 17:04:48 +0800

    fix trans

--- lisp/google-translate.el
+++ lisp/google-translate.el
@@ -1,14 +1,15 @@
-(defun trans-text (cmd beg end)
+(defun transp (cmd beg end)
   (interactive "r")
-  (concat cmd " \"" (replace-regexp-in-string "`" "\\`" (buffer-substring beg end) nil t) "\""))
+  (message "%s" (shell-command-to-string (concat 
+	cmd " \"" (replace-regexp-in-string "`" "\\`" (buffer-substring beg end) nil t) "\""))))
 
 (defun translation-comment (beg end)
   (interactive "r")
-  (message "%s" (shell-command-to-string (trans-text "~/.emacs.d/bin/transc" beg end))))
+  (transp "~/.emacs.d/bin/transc" beg end))
 
-(defun translation-text (beg end)
+(defun translation-paragraph (beg end)
   (interactive "r")
-  (message "%s" (shell-command-to-string (trans-text "~/.emacs.d/bin/trans" beg end))))
+  (transp "~/.emacs.d/bin/transp" beg end))
 
 (defun translation-word (beg end)
   (interactive "r")
@@ -18,7 +19,7 @@
 (defvar google-translate-mode-keymap
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c c") 'translation-comment)
-    (define-key map (kbd "C-c t") 'translation-text)
+    (define-key map (kbd "C-c t") 'translation-paragraph)
     (define-key map (kbd "C-c w") 'translation-word) map))
 
 (define-minor-mode google-translate-mode
